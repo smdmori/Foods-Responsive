83c27bfbc01a489df73a858c7a6447015094c6cc
lamassab <m.smd1371@gmail.com>
2021-08-22 17:58:45 +0430

    End of building navbar

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+
+// MORI's Way for animating navbar
+
+// const menu = document.querySelector('.menu')
+// const navbar = document.querySelector('.navbar')
+// let condition = 0
+// menu.addEventListener('click', toggleMenu)
+
+// function toggleMenu() {
+
+//     if ( !condition ) {
+//         document.querySelector('.navbar').style.top = 0
+//         document.querySelector('.menu').style.top = '15rem'
+
+//         condition = 1
+//     } else {
+//         document.querySelector('.navbar').style.top = '-13.8rem'
+//         document.querySelector('.menu').style.top = '1rem'
+
+//         condition = 0
+//     }
+// }
+
+// End of MORI's Way
+
+document.querySelector('.menu').addEventListener('click', () => {
+    document.querySelectorAll('.target').forEach((item) => {
+        item.classList.toggle('change')
+    })
+})
+
 const icons = document.querySelectorAll('.section-1-icons i')
 let i = 1
 
